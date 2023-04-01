f681d27fcd2899d1591ecdf9313565bd85313af0
Olvine23 <olivinegeorge@gmail.com>
2023-04-02 00:09:47 +0300

    Added stats page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Form from "./components/form";
+import Stats from "./pages/stats";
 
 function App() {
   
   return (
     <Routes>
        <Route path="/" element={<Form />} />
+       <Route path="/stats" element={<Stats />} />
         {/* <Route path="/" element={<AllNotes images={images} term={term} setTerm={setTerm} />} /> */}
         {/* <Route path="/note/:id" element={<SingleNote />} /> */}
       </Routes>
