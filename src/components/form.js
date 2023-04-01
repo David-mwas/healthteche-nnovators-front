e34b02dff2abfc561aac7fdcb6c0b38544f221f8
Olvine23 <olivinegeorge@gmail.com>
2023-04-01 16:25:02 +0300

    Changed input type from email to text

--- src/components/form.js
+++ src/components/form.js
@@ -14,14 +14,14 @@ const Form = () => {
           <form action="#" class="space-y-8">
             <div>
               <label
-                for="email"
+                for="text"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
               >
                 Facility Name and Contact Details
               </label>
               <input
-                type="email"
-                id="email"
+                type="text"
+                id="text"
                 class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="enter facility name"
                 required
