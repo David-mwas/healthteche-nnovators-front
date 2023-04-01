e396cae81f229529b0ecf7ff1650b1da69d5f6c6
Olvine23 <olivinegeorge@gmail.com>
2023-04-01 16:04:01 +0300

    Removed unused vars

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import "./App.css";
-import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Form from "./components/form";
  import SingleNote from "./pages/singlenote";
 function App() {
-  const [images, setImages] = useState([]);
-  const [term, setTerm] = useState("");
-
-  useEffect(() => {
-    getNotes();
-  }, []);
-  let getNotes = async () => {
-    const response = await fetch(`/api/notes/`);
-    const data = await response.json();
-    console.log(data);
-    setImages(data);
-  };
-
+  
   return (
     <Routes>
        <Route path="/" element={<Form />} />
